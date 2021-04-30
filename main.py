a0de6cdf405bd2a6e63fb2d34dc94a855f3e5c20
Danilkusss <54574995+Danilkusss@users.noreply.github.com>
2021-04-30 16:09:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import vk_api
-import vk
 import time
+
 
 texts = ["i'm good", "Nice", "я люблю кушать", "блин,этот статус меняется каждые 30 секунд",
          "я крутой)", "черттт,я программист", "обнови страничку и посмотри снова на стутс)", "жизнь так прекрасна)"
@@ -12,29 +12,49 @@ i = 1
 result = input("ты хочешь запустить авто статус? ")
 
 if result == "да":
-    vk_session = vk_api.VkApi('Login', 'Passworld')
+    vk_session = vk_api.VkApi('Login', 'pass')
     vk_session.auth()
     i = 1
     vk = vk_session.get_api()
-    while True:
-        for text in texts:
-            i = i + 1
-            print("статус приянт успешно,начинаме смену задоного статуса")
-            time.sleep(3)
-            print("статус сменился на: ", text)
-            print("количество изменений прошло: ", i)
-            vk.status.set(text=text)
-            time.sleep(27)
+
+    try:
+        while True:
+            for text in texts:
+                i = i + 1
+                print("статус приянт успешно,начинаме смену задоного статуса")
+                time.sleep(3)
+                print("статус сменился на: ", text)
+                print("количество изменений прошло: ", i)
+                vk.status.set(text=text)
+                time.sleep(27)
+    except vk_api.exceptions.Captcha as captcha:
+        captcha.sid  # Получение sid
+        captcha.get_url()  # Получить ссылку на изображение капчи
+        captcha.get_image()
+
+
+
+    # while True:
+    #     for text in texts:
+    #         i = i + 1
+    #         print("статус приянт успешно,начинаме смену задоного статуса")
+    #         time.sleep(3)
+    #         print("статус сменился на: ", text)
+    #         print("количество изменений прошло: ", i)
+    #         vk.status.set(text=text)
+    #         time.sleep(27)
+
+
 else:
     print("завершение")
 
 
-vk_session = vk_api.VkApi('log', 'pss')
-vk_session.auth()
-
-vk = vk_session.get_api()
-
-while True:
-    for text in texts:
-        vk.status.set(text=text)
-        time.sleep(30)
+# vk_session = vk_api.VkApi('', '')
+# vk_session.auth()
+#
+# vk = vk_session.get_api()
+#
+# while True:
+#     for text in texts:
+#         vk.status.set(text=text)
+#         time.sleep(30)
